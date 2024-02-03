a1295d8f156d098fb4f790a1b435275c4388d2e5
Praveen Nath <pnath6745@gmail.com>
2024-02-03 10:00:00 +0530

    Day 34, Date 2024-02-03, Added ./sorting/BubbleSortDemo.java

---
+++ sorting/BubbleSortDemo.java
@@ -0,0 +1,32 @@
+package com.oops.sorting;
+
+import java.util.Arrays;
+
+/**
+ * Class demonstrating the Bubble Sort algorithm
+ */
+public class BubbleSortDemo {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Sorted array:   " + Arrays.toString(arr));
+    }
+} 
