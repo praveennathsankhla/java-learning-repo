3e6e9012b35ed432b2cfe343c9614c0453694f16
Praveen Nath <pnath6745@gmail.com>
2024-02-11 08:35:00 +0530

    Day 6, Date 2024-02-11, Added Car.java

---
+++ Car.java
@@ -0,0 +1,28 @@
+package com.oops.classes;
+
+/**
+ * Car class with brand, model, and year attributes
+ */
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Car Information:");
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("Toyota", "Camry", 2020);
+        car.displayInfo();
+    }
+} 
